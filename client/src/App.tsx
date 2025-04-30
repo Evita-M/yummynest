5616cbb58abc6585c808121f0a9d12622aae2c3b
Eva Matova <eva.matova6@gmail.com>
2025-04-30 23:31:57 +0300

    feat: Add ProductPage route to App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import RecipesPage from './pages/RecipesPage'
 import HomePage from './pages/HomePage'
 import ProductsPage from './pages/ProductsPage'
 import CartPage from './pages/CartPage'
+import ProductPage from './pages/ProductPage'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='/recipes' element={<RecipesPage />} />
         <Route path='/products' element={<ProductsPage />} />
+        <Route path='/products/:id' element={<ProductPage />} />
         <Route path='/cart' element={<CartPage />} />
       </Routes>
     </>
