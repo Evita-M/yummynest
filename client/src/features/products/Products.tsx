ff167802c14d74215efe99f967559a0f428f8edb
Eva Matova <eva.matova6@gmail.com>
2025-05-10 17:02:56 +0300

    refactor: Remove unused props from Products component to streamline functionality

--- client/src/features/products/Products.tsx
+++ client/src/features/products/Products.tsx
@@ -8,7 +8,6 @@ import {
   selectAllCartItems,
 } from '@/features/cart/cartSlice';
 import { Loader } from '@/components/loader/Lodaer';
-import { PageHeader } from '@/components/page-header/PageHeader';
 
 interface Product {
   id: string;
@@ -19,12 +18,10 @@ interface Product {
 }
 
 interface ProductsProps {
-  title: string;
-  count: number;
   items: Product[];
 }
 
-export const Products: FC<ProductsProps> = ({ title, count, items }) => {
+export const Products: FC<ProductsProps> = ({ items }) => {
   const dispatch = useAppDispatch();
   const cartItems = useAppSelector(selectAllCartItems);
   const { status } = useAppSelector((state) => state.products);
@@ -46,29 +43,27 @@ export const Products: FC<ProductsProps> = ({ title, count, items }) => {
       {status === 'loading' ? (
         <Loader />
       ) : (
-        <>
-          <div className='grid grid-cols-2 gap-[3.2rem] md:grid-cols-3 lg:grid-cols-4'>
-            {items.map(({ id, name, price, offerPrice, inStock }: Product) => {
-              const quantity = getCartItemInfo(id);
-              return (
-                <ProductCard
-                  key={id}
-                  name={name}
-                  price={price.toFixed(2)}
-                  offerPrice={offerPrice.toFixed(2)}
-                  onClick={() =>
-                    dispatch(addToCart({ id, name, price, offerPrice }))
-                  }
-                  onIncrement={() => handleIncrement(id)}
-                  onDecrement={() => handleDecrement(id)}
-                  inCartQuantity={quantity}
-                  href={`/products/${id}`}
-                  inStock={inStock}
-                />
-              );
-            })}
-          </div>
-        </>
+        <div className='grid grid-cols-2 gap-[3.2rem] md:grid-cols-3 lg:grid-cols-4'>
+          {items.map(({ id, name, price, offerPrice, inStock }: Product) => {
+            const quantity = getCartItemInfo(id);
+            return (
+              <ProductCard
+                key={id}
+                name={name}
+                price={price.toFixed(2)}
+                offerPrice={offerPrice.toFixed(2)}
+                onClick={() =>
+                  dispatch(addToCart({ id, name, price, offerPrice }))
+                }
+                onIncrement={() => handleIncrement(id)}
+                onDecrement={() => handleDecrement(id)}
+                inCartQuantity={quantity}
+                href={`/products/${id}`}
+                inStock={inStock}
+              />
+            );
+          })}
+        </div>
       )}
     </section>
   );
