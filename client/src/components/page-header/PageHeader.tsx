b4ed839a15151381f375bfc961189bd088a7dfa6
Eva Matova <eva.matova6@gmail.com>
2025-05-10 16:41:27 +0300

    refactor: Make description prop optional

--- client/src/components/page-header/PageHeader.tsx
+++ client/src/components/page-header/PageHeader.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 
 interface PageHeaderProps {
   title: string;
-  description: string;
+  description?: string;
   className?: string;
 }
 
@@ -15,7 +15,9 @@ export const PageHeader: FC<PageHeaderProps> = ({
   return (
     <div className={clsx('flex flex-wrap items-end gap-[1.6rem]', className)}>
       <h1 className='!mb-[0] capitalize'>{title}</h1>
-      {description && <p className='text-blue font-medium'>{description}</p>}
+      {description && (
+        <p className='text-blue pb-[0.2rem] font-medium'>{description}</p>
+      )}
     </div>
   );
 };
