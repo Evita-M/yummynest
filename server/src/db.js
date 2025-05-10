3cc8bf60ccaf82cf90c90c40e6b849892e252a3d
Eva Matova <eva.matova6@gmail.com>
2025-05-10 18:33:10 +0300

    feat: Implement database seeding and category management with updated product structure

--- server/src/db.js
+++ server/src/db.js
@@ -1,6 +1,12 @@
 import sqlite3 from 'sqlite3';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const sql3 = sqlite3.verbose();
-const dbName = './src/yummynest.db';
+const dbName = path.join(__dirname, 'yummynest.db');
 
 const DB = new sql3.Database(
   dbName,
@@ -15,15 +21,31 @@ function connected(err) {
   console.log('Successfully connected to the SQLite database (yummynest.db).');
 }
 
-let sql = `CREATE TABLE IF NOT EXISTS products (
+let sql = `CREATE TABLE IF NOT EXISTS categories (
+  id TEXT PRIMARY KEY,
+  name TEXT NOT NULL,
+  createdAt TEXT NOT NULL,
+  updatedAt TEXT NOT NULL
+)`;
+
+DB.run(sql, [], (err) => {
+  if (err) {
+    return new Error('Error creating categories table');
+  }
+  console.log('Categories table created or already existed');
+});
+
+sql = `CREATE TABLE IF NOT EXISTS products (
   id TEXT PRIMARY KEY,
   name TEXT NOT NULL,
   price REAL NOT NULL,
   offerPrice REAL,
   description TEXT,
+  categoryId TEXT NOT NULL,
   createdAt TEXT NOT NULL,
   updatedAt TEXT NOT NULL,
-  inStock BOOLEAN NOT NULL
+  inStock BOOLEAN NOT NULL,
+  FOREIGN KEY (categoryId) REFERENCES categories(id)
 )`;
 
 DB.run(sql, [], (err) => {
