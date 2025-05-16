31d48e19f9bfa0f2d55b53d997f8e51f68d38a7b
Eva Matova <eva.matova6@gmail.com>
2025-05-16 22:30:50 +0300

    fix: Update welcome message on Home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -5,9 +5,7 @@ const HomePage = () => {
     <section className='relative h-[540px] w-full'>
       <div className='shadow-soft absolute left-[50%] top-[50%] flex translate-x-[-50%] translate-y-[-50%] flex-col items-center justify-center rounded-[1.2rem] bg-white p-12 text-center'>
         <h1 className='!mb-[1.6rem]'>Welcome to YummyNest</h1>
-        <p className='text-4xl'>
-          Find it. Cook it. Love it. Your recipe journey starts here.
-        </p>
+        <p>Your favorite grocery e-shop is here. Discover, taste, and enjoy.</p>
       </div>
       <img src={hero} alt='hero' className='h-full w-full object-cover' />
     </section>
